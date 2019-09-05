f29090fe8c310c7be1a8d4b266aeeeb2c00ef0e5
Fisayo <aikomofisayo@gmail.com>
2019-09-05 16:05:27 +0100

    Completed the reveal onscroll behaviour

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 // var Person = require('./modules/Person'); // this is for NodeJS
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery'
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
